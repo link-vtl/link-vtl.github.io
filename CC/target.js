27da21d994e24ac2e32c01d9b0290dc636267cc1
zeth <dubo0720@vandals.uidaho.edu>
2019-11-03 17:57:49 -0800

    parm passing first success

---
+++ CC/target.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+
+// Get the URL & Params, Break them Down
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+  }
+
+// Get just the params
+var FID_ENC = getUrlVars()["FID"];
+var SID_ENC = getUrlVars()["SID"];
+// Decode the URI to Plaintext Chars
+var FID = decodeURI(FID_ENC);
+var SID = decodeURI(SID_ENC);
+
+if(SID == "undefined") {
+	$('label[for=siteID], input#siteID').hide();
+	var GoTo = "https://www.youtube.com/watch?v="+FID;
+}
+
+if(FID == "undefined") {
+	$('label[for=flashID], input#flashID').hide();
+	var GoTo = "https://www.youtube.com/watch?v="+SID;
+} 
+
+// Write it to the Form Field (js only)
+document.getElementById('flashID').value = FID;
+document.getElementById('siteID').value = SID;
+
+document.getElementById("go").onclick = function () {
+        location.href = GoTo;
+}
+// Write it to the Form Field (jquery targeted by name) 
+//$("input[type='text'][name=eventInputField2]").val(eventID);
+//$("input[type='text'][name=eventInputField2]").val(eventID);
+  
+  
+  
+  }); //END DOCREADY
--- test/README.markdown
+++
@@ -1,4 +0,0 @@
-# Get URL Parameters and Fill them in a Form Field
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/dustyyeti/pen/abbqOwg](https://codepen.io/dustyyeti/pen/abbqOwg).
-
- A few simple lines of Javascript to get the values of parameters from a URL, and fill them into a form field(?event=Event Name). It also decodes URI's to have clean inputs (i.e. no '%20').
