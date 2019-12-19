a1576b40c3b52f0aa5a2b4c198851cdbd615715b
Jackson Schacher <jschacher8@gmail.com>
2019-12-19 13:43:47 -0800

    Fall 19 Final Update
    Added Unity Project to Git Repo
    
    Virtual Buttons
    UI elements
    
    Vuforia Support
    
    Map Trackers
    
    Interactive Scripts to be customized for next semester.

---
+++ AR Overlay Map/Assets/ChangeImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour 
+{
+    public Sprite[] images;
+    public Image imageContainer;
+    // Start is called before the first frame update
+    public void SetImage(int index)
+        {
+            if (images.Length >= index)
+            {
+                imageContainer.sprite = images[index];
+            } else
+            {
+                Debug.LogError("Invalid image index: " + index);
+            }
+        }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
